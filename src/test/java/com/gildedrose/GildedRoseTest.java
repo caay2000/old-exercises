368523b190babdac610f5271cdcb218648c76884
alc20 <SkySol03>
2019-04-27 00:11:26 +0200

    * first 3 tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,19 +1,43 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
+    public void sellIn_decreases_gradually() {
+        Item item = anItem("foo", 10, 10);
 
-        app.updateQuality();
+        aGildedRose(item).updateQuality();
 
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(9, item.sellIn);
     }
 
+    @Test
+    public void quality_decreases_gradually() {
+        Item item = anItem("foo", 10, 10);
+
+        aGildedRose(item).updateQuality();
+
+        assertEquals(9, item.quality);
+    }
+
+    @Test
+    public void quality_decreases_twice_as_fast_when_sellIn_expired() {
+        Item item = anItem("foo", 0, 10);
+
+        aGildedRose(item).updateQuality();
+
+        assertEquals(8, item.quality);
+    }
+
+    private Item anItem(String name, int sellIn, int quality) {
+        return new Item(name, sellIn, quality);
+    }
+
+    private GildedRose aGildedRose(Item... items) {
+        return new GildedRose(items);
+    }
 }
