4c255730cd39630ca1df9467bffd994fe4efa3af
alc20 <SkySol03>
2019-04-27 02:08:21 +0200

    * product refactor

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,65 +1,63 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 class GildedRose {
 
     private static final String AGED_BRIE = "Aged Brie";
     private static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
     private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
-    private static final int MAX_ITEM_QUALITY = 50;
-    private static final int MIN_ITEM_QUALITY = 0;
-
-    private Item[] items;
+    private List<Product> products;
 
     public GildedRose(Item[] items) {
-        this.items = items;
+        products = Arrays.asList(items).stream()
+                .map(e -> new Product(e))
+                .collect(Collectors.toList());
     }
 
     public void updateQuality() {
-        for (Item item : items) {
-            if (item.name.equals(AGED_BRIE) || item.name.equals(BACKSTAGE_PASS)) {
-                if (item.quality < MAX_ITEM_QUALITY) {
-                    item.quality = item.quality + 1;
+        for (Product product : products) {
 
-                    if (item.name.equals(BACKSTAGE_PASS)) {
+            processExpireDate(product);
+            processQuality(product);
+        }
+    }
 
-                        if (item.quality < MAX_ITEM_QUALITY) {
-                            if (item.sellIn < 11) {
-                                item.quality = item.quality + 1;
-                            }
-                            if (item.sellIn < 6) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
+    private void processQuality(Product product) {
+        if (product.getName().equals(SULFURAS)) {
+            return;
+        }
+        if (product.getName().equals(AGED_BRIE)) {
+            product.increaseQuality();
+            if (product.isExpired()) {
+                product.increaseQuality();
+            }
+        } else if (product.getName().equals(BACKSTAGE_PASS)) {
+            if (product.isExpired()) {
+                product.killQuality();
             } else {
-                if (!item.name.equals(SULFURAS)) {
-                    if (item.quality > MIN_ITEM_QUALITY) {
-                        item.quality = item.quality - 1;
-                    }
+                product.increaseQuality();
+                if (product.getDaysToExpire() < 10) {
+                    product.increaseQuality();
+                }
+                if (product.getDaysToExpire() < 5) {
+                    product.increaseQuality();
                 }
             }
-
-            if (!item.name.equals(SULFURAS)) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < MIN_ITEM_QUALITY) {
-                if (item.name.equals(AGED_BRIE)) {
-                    if (item.quality < MAX_ITEM_QUALITY) {
-                        item.quality = item.quality + 1;
-                    }
-                } else {
-                    if (item.name.equals(BACKSTAGE_PASS)) {
-                        item.quality = item.quality - item.quality;
-                    } else {
-                        if (item.quality > MIN_ITEM_QUALITY) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                }
+        } else {
+            product.decreaseQuality();
+            if (product.isExpired()) {
+                product.decreaseQuality();
             }
         }
+    }
+
+    private void processExpireDate(Product product) {
+        if (product.getName().equals(SULFURAS)) {
+            return;
+        }
+        product.expireOneDay();
     }
 }
--- src/main/java/com/gildedrose/Product.java
+++ src/main/java/com/gildedrose/Product.java
@@ -2,21 +2,50 @@ package com.gildedrose;
 
 public class Product {
 
+    private static final int EXPIRE_DAY = 0;
+
+    private static final int MIN_ITEM_QUALITY = 0;
+    private static final int MAX_ITEM_QUALITY = 50;
+
     private Item item;
 
     public Product(Item item) {
         this.item = item;
     }
 
+    public String getName() {
+        return this.item.name;
+    }
+
+    public int getQuality() {
+        return this.item.quality;
+    }
+
+    public int getDaysToExpire() {
+        return this.item.sellIn;
+    }
+
+    public boolean isExpired() {
+        return this.item.sellIn < EXPIRE_DAY;
+    }
+
+    public void expireOneDay() {
+        this.item.sellIn--;
+    }
+
     public void decreaseQuality() {
-        this.item.quality--;
+        if (getQuality() > MIN_ITEM_QUALITY) {
+            this.item.quality--;
+        }
     }
 
     public void increaseQuality() {
-        this.item.quality++;
+        if (getQuality() < MAX_ITEM_QUALITY) {
+            this.item.quality++;
+        }
     }
 
-    public void updateSellIn() {
-        this.item.sellIn--;
+    public void killQuality() {
+        this.item.quality = 0;
     }
 }
