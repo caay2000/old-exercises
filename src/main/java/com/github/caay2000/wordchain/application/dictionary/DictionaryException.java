d59a714b021ce2f4beeb066d445b08af398c2d1b
caay2000@gmail.com <SkySol03>
2019-05-22 22:36:25 +0200

    * clean up

--- src/main/java/com/github/caay2000/wordchain/application/dictionary/DictionaryException.java
+++
@@ -1,8 +0,0 @@
-package com.github.caay2000.wordchain.application.dictionary;
-
-public class DictionaryException extends RuntimeException {
-
-    public DictionaryException(Throwable cause) {
-        super(cause.getMessage(), cause);
-    }
-}
