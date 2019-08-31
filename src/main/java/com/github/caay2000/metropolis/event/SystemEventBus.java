833fb01ba9aa50f92af0e3263440d547e15447d7
caay2000 <caay2000@gmail.com>
2019-09-01 00:58:08 +0200

    * multi event * fix not needed stream

--- src/main/java/com/github/caay2000/metropolis/event/SystemEventBus.java
+++ src/main/java/com/github/caay2000/metropolis/event/SystemEventBus.java
@@ -1,23 +1,30 @@
 package com.github.caay2000.metropolis.event;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 import java.util.function.Consumer;
+
+import static java.util.Objects.isNull;
 
 public final class SystemEventBus implements EventBus {
 
-    private final HashMap<EventType, Consumer<? super Event>> subscriptions;
+    private final HashMap<EventType, Set<Consumer<? super Event>>> subscriptions;
 
     public SystemEventBus() {
         this.subscriptions = new HashMap<>();
     }
 
     public void publish(Event event) {
-        subscriptions.entrySet().stream()
-                .filter(entry -> event.getType().equals(entry.getKey()))
-                .forEach(entry -> entry.getValue().accept(event));
+        subscriptions.get(event.getType()).stream()
+                .forEach(e -> e.accept(event));
     }
 
     public void subscribe(EventType eventType, Consumer<? super Event> function) {
-        this.subscriptions.put(eventType, function);
+        if (isNull(this.subscriptions.get(eventType))) {
+            this.subscriptions.put(eventType, new HashSet<>());
+        }
+        this.subscriptions.get(eventType).add(function);
     }
 }
